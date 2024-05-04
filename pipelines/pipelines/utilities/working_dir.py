632abb2ac6938aa327368fd9675fe9442133d7de
bryce <brycekbargar@gmail.com>
2024-05-04 17:16:22 +0000

    Create a file system abstraction

---
+++ pipelines/pipelines/utilities/working_dir.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import re
+
+
+class WorkingDirectory:
+    """Abstracts away file path operations.
+
+    Probably should be instantiated using for_metaflow_run
+    unless you know what you're doing.
+    """
+
+    def __init__(self, path: Path):
+        """Creates a new WorkingDirectory."""
+
+        self._path = path
+        self._path.mkdir(mode=755, parents=True, exist_ok=True)
+
+    @classmethod
+    def for_metaflow_run(cls, base: Path, run_id: int) -> "WorkingDirectory":
+        """Creates a new WorkingDirectory for a Metaflow run."""
+
+        return cls(base / str(run_id))
+
+    def push_segment(self, segment: str) -> "WorkingDirectory":
+        """Creates a new subdirectory of the WorkingDirectory."""
+
+        return WorkingDirectory(self._path / segment)
+
+    def push_partitions(self, *args: tuple[str, str]) -> "WorkingDirectory":
+        """Creates a new subdirectory of the WorkingDirectory.
+
+        The subdirectory name will be based on the passed key/value pairs
+        allowing for partitioning data based on its content.
+        """
+
+        return WorkingDirectory(self._path / "&".join([f"{k}={v}" for (k, v) in args]))
+
+    def glob_keys(self, file_glob: str, *args: str) -> str:
+        """Gets the current WorkingDirectory + given file_glob as a string.
+
+        Passing keys will additionally glob the value of those keys in the partitioning scheme."""
+
+        glob = str(self._path / file_glob)
+        for k in args:
+            glob = re.sub(rf"({k})=\w*", r"\1=*", glob)
+        return glob
+
+    def file(self, file: str) -> str:
+        """Gets the current WorkingDirectory + given file as a string."""
+
+        return str(self._path / file)
+
+    def __str__(self):
+        return str(self._path)
---
+++ pipelines/tests/utilities/test_working_dir.py
@@ -0,0 +1,54 @@
+from unittest.mock import patch, MagicMock
+from pathlib import Path
+
+from pipelines.utilities.working_dir import WorkingDirectory as uut
+
+
+class TestWorkingDirectory:
+    @patch.object(Path, "mkdir")
+    def test_dirs_are_created(self, mock_exists: MagicMock):
+        uut(Path("path"))
+        uut.for_metaflow_run(Path("base"), 123)
+
+        assert mock_exists.call_count == 2
+
+    @patch.object(Path, "mkdir")
+    def test_segments(self, _: MagicMock):
+        base = uut.for_metaflow_run(Path("/base"), 123)
+        segment1 = base.push_segment("segment1")
+        segment2 = base.push_segment("segment").push_segment("two")
+
+        assert base.file("base.txt") == "/base/123/base.txt"
+        assert segment1.file("segment1.txt") == "/base/123/segment1/segment1.txt"
+        assert segment2.file("segment2.txt") == "/base/123/segment/two/segment2.txt"
+
+    @patch.object(Path, "mkdir")
+    def test_partitions(self, _: MagicMock):
+        base = uut.for_metaflow_run(Path("/base"), 123)
+        partition1 = base.push_partitions(("key1", "value1"))
+        partition2 = base.push_partitions(("key1", "value1"), ("key2", "value2"))
+        partition3 = partition1.push_partitions(("key2", "value2"))
+
+        assert base.glob_keys("base.parquet") == "/base/123/base.parquet"
+        assert (
+            partition1.glob_keys("partition1.parquet")
+            == "/base/123/key1=value1/partition1.parquet"
+        )
+        assert (
+            partition2.glob_keys("partition2.parquet")
+            == "/base/123/key1=value1&key2=value2/partition2.parquet"
+        )
+        assert (
+            partition3.glob_keys("partition3.parquet")
+            == "/base/123/key1=value1/key2=value2/partition3.parquet"
+        )
+
+        assert partition1.glob_keys("*.parquet", "key1") == "/base/123/key1=*/*.parquet"
+        assert (
+            partition2.glob_keys("*.parquet", "key2")
+            == "/base/123/key1=value1&key2=*/*.parquet"
+        )
+        assert (
+            partition3.glob_keys("*.parquet", "key1", "key2")
+            == "/base/123/key1=*/key2=*/*.parquet"
+        )
